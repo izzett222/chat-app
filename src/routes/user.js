7cbaf70545a28826afd527d8166237f7a9d6a983
unknown <iizzeddin62@gmail.com>
2020-11-10 17:24:17 +0200

    ft(profile): get user profile
    - add profile controller
    - add test for profile controller
    - add protect route middleware

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { validateSignup, validateLogin } from '../validation/userValidate';
-import { signupController, loginController } from '../controllers/userController';
+import { signupController, loginController, profileController } from '../controllers/userController';
+import protect from '../middleware/protectRoute';
 
 const router = express.Router();
 router.post('/signup', validateSignup, signupController);
 router.post('/login', validateLogin, loginController);
-
+router.get('/profile', protect, profileController);
 export default router;
